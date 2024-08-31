577c3d9def5578229998424bdb1f89d2ab33df4e
AntonyOnScript <santhiagoafk@gmail.com>
2024-08-30 22:00:38 -0300

    fix: increase base 64 limit

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY
 
 console.log('GEMINI_API_KEY: ', GEMINI_API_KEY)
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ limit: '80mb', extended: true }))
+app.use(express.json({ limit: '80mb' }))
 app.use(router)
 
 app.listen(3000, () => {
